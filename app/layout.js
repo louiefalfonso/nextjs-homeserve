1d3ea2b8a465cd402513a4a1d256aa19bd930a24
Louie Alfonso <lfriasalfonso@gmail.com>
2024-04-04 14:31:11 +0100

    HomeServe Construction Project
    HomeServe Construction Project

--- app/layout.js
+++ app/layout.js
@@ -17,7 +17,7 @@ export default function RootLayout({ children }) {
         <link rel="preconnect" href="https://fonts.googleapis.com"/>
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
         <link href="https://fonts.googleapis.com/css2?family=Epilogue:wght@400;500;600;700;800&family=Rubik:wght@400;500;600;700;800;900&display=swap" rel="stylesheet"/>
-
+        <link rel="icon" type="image/png" href="img/favicon.png"></link>
         <link rel="stylesheet" href="css/bootstrap.min.css"/>
         <link rel="stylesheet" href="css/fontawesome.min.css"/>
         <link rel="stylesheet" href="css/magnific-popup.min.css"/>
--- components/MobileMenu.js
+++ components/MobileMenu.js
@@ -10,133 +10,17 @@ export default function MobileMenu() {
             </button>
             <div className="mobile-logo">
             <a href="index.html">
-                <img src="img/logo-black.png" alt="Consik" className="logo" />
+                <img src="img/logo-homeserve.png" alt="Consik" className="logo" />
             </a>
             </div>
             <div className="vs-mobile-menu">
             <ul>
-                <li className="menu-item-has-children">
-                <a href="index.html">Home</a>
-                <ul className="sub-menu">
-                    <li>
-                    <a href="index.html">Home 1</a>
-                    </li>
-                    <li>
-                    <a href="index-2.html">Home 2</a>
-                    </li>
-                    <li>
-                    <a href="index-3.html">Home 3</a>
-                    </li>
-                </ul>
-                </li>
-                <li>
-                <a href="about.html">About Us</a>
-                </li>
-                <li className="menu-item-has-children">
-                <a href="service.html">Services</a>
-                <ul className="sub-menu">
-                    <li>
-                    <a href="services.html">Service</a>
-                    </li>
-                    <li>
-                    <a href="service-details.html">Service Details</a>
-                    </li>
-                </ul>
-                </li>
-                <li className="menu-item-has-children">
-                <a href="#none">Pages</a>
-                <ul className="sub-menu">
-                    <li>
-                    <a href="index.html">Home 1</a>
-                    </li>
-                    <li>
-                    <a href="index-2.html">Home 2</a>
-                    </li>
-                    <li>
-                    <a href="index-3.html">Home 3</a>
-                    </li>
-                    <li>
-                    <a href="shop.html">Shop</a>
-                    </li>
-                    <li>
-                    <a href="shop-details.html">Shop Details</a>
-                    </li>
-                    <li>
-                    <a href="faq.html">FAQ's</a>
-                    </li>
-                    <li>
-                    <a href="blog.html">Blog List</a>
-                    </li>
-                    <li>
-                    <a href="blog-grid.html">Blog Grid</a>
-                    </li>
-                    <li>
-                    <a href="blog-details.html">Blog Details</a>
-                    </li>
-                    <li>
-                    <a href="services.html">Service</a>
-                    </li>
-                    <li>
-                    <a href="service-details.html">Service Details</a>
-                    </li>
-                    <li>
-                    <a href="team.html">Team</a>
-                    </li>
-                    <li>
-                    <a href="team-details.html">Team Details</a>
-                    </li>
-                    <li>
-                    <a href="project.html">Projects</a>
-                    </li>
-                    <li>
-                    <a href="project-details.html">Projects Details</a>
-                    </li>
-                    <li>
-                    <a href="contact.html">Contact Us</a>
-                    </li>
-                    <li>
-                    <a href="error.html">Error Page</a>
-                    </li>
-                    <li>
-                    <a href="comming-soon.html">Comming Soon</a>
-                    </li>
-                    <li>
-                    <a href="account.html">My Account</a>
-                    </li>
-                    <li>
-                    <a href="cart.html">Cart</a>
-                    </li>
-                    <li>
-                    <a href="checkout.html">Checkout</a>
-                    </li>
-                    <li>
-                    <a href="price.html">Pricing Plan</a>
-                    </li>
-                    <li>
-                    <a href="element-typography.html">Elements Typography</a>
-                    </li>
-                    <li>
-                    <a href="element-buttons.html">Button Elements</a>
-                    </li>
-                </ul>
-                </li>
-                <li className="menu-item-has-children">
-                <a href="blog.html">News</a>
-                <ul className="sub-menu">
-                    <li>
-                    <a href="blog.html">Blog List</a>
-                    </li>
-                    <li>
-                    <a href="blog-grid.html">Blog Grid</a>
-                    </li>
-                    <li>
-                    <a href="blog-details.html">Blog Details</a>
-                    </li>
-                </ul>
-                </li>
-                <li>
-                <a href="contact.html">Contact Us</a>
-                </li>
+                <li><a href="index.html">Home</a></li>
+                <li><a href="about.html">About</a></li>
+                <li><a href="services.html">Services</a></li>
+                <li><a href="#">Pages</a></li>
+                <li><a href="blog.html">News</a></li>
+                <li><a href="contact.html">Contact</a></li>
             </ul>
             </div>
         </div>
