e8acd3ae2567e9c9a5d3c3ba9b34e2c013082b58
Louie Alfonso <lfriasalfonso@gmail.com>
2024-04-12 22:31:48 +0100

    HomeServe Construction Project

--- app/projects/page.js
+++ app/projects/page.js
@@ -1,7 +1,31 @@
+import ProjectGrid from '@/components/ProjectGrid'
+import RequestQuote from '@/components/RequestQuote'
 import React from 'react'
 
 export default function page() {
   return (
-    <div>Projects Page</div>
+    <>
+    <div className="breadcumb-wrapper" data-bg-src="img/breadcumb/breadcumb-bg.jpg">
+      <div className="container z-index-common">
+        <div className="breadcumb-content">
+          <h1 className="breadcumb-title">OUR PROJECTS</h1>
+          <p className="breadcumb-text">
+            construction and architecture environmentally most responsible for any
+            kinds of themes.
+          </p>
+          <div className="breadcumb-menu-wrap">
+            <ul className="breadcumb-menu">
+              <li>
+                <a href="index.html">Home</a>
+              </li>
+              <li>OUR PROJECTS</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+    <ProjectGrid/>
+    <RequestQuote/>
+    </>
   )
 }
---
+++ components/ProjectGrid.js
@@ -0,0 +1,223 @@
+import React from 'react'
+
+export default function ProjectGrid() {
+  return (
+    <>
+    <div className="project--layout4 space-top space-extra-bottom">
+        <div className="container">
+            <div className="row gx-70">
+            <div className="col-lg-6 col-md-6">
+                <div className="project-block--style4">
+                <div className="project-block__media">
+                    <a href="project-details.html">
+                    <img
+                        className="project-block__img"
+                        src="img/project/project-4-1.jpg"
+                        alt="project details"
+                    />
+                    </a>
+                </div>
+                <div className="project-block__content">
+                    <span className="project-block__subtitle">CONSTRUCTION</span>
+                    <h3 className="project-block__title h4">
+                    <a
+                        className="project-block__title__link"
+                        href="project-details.html"
+                    >
+                        Life Science Center
+                    </a>
+                    </h3>
+                </div>
+                </div>
+                <div className="project-block--style4">
+                <div className="project-block__media">
+                    <a href="project-details.html">
+                    <img
+                        className="project-block__img"
+                        src="img/project/project-4-2.jpg"
+                        alt="project details"
+                    />
+                    </a>
+                </div>
+                <div className="project-block__content">
+                    <span className="project-block__subtitle">CONSTRUCTION</span>
+                    <h3 className="project-block__title h4">
+                    <a
+                        className="project-block__title__link"
+                        href="project-details.html"
+                    >
+                        Oil Mill Construction
+                    </a>
+                    </h3>
+                </div>
+                </div>
+                <div className="project-block--style4">
+                <div className="project-block__media">
+                    <a href="project-details.html">
+                    <img
+                        className="project-block__img"
+                        src="img/project/project-4-3.jpg"
+                        alt="project details"
+                    />
+                    </a>
+                </div>
+                <div className="project-block__content">
+                    <span className="project-block__subtitle">CONSTRUCTION</span>
+                    <h3 className="project-block__title h4">
+                    <a
+                        className="project-block__title__link"
+                        href="project-details.html"
+                    >
+                        Building Construction
+                    </a>
+                    </h3>
+                </div>
+                </div>
+                <div className="project-block--style4">
+                <div className="project-block__media">
+                    <a href="project-details.html">
+                    <img
+                        className="project-block__img"
+                        src="img/project/project-4-4.jpg"
+                        alt="project details"
+                    />
+                    </a>
+                </div>
+                <div className="project-block__content">
+                    <span className="project-block__subtitle">CONSTRUCTION</span>
+                    <h3 className="project-block__title h4">
+                    <a
+                        className="project-block__title__link"
+                        href="project-details.html"
+                    >
+                        Laminate Flooring
+                    </a>
+                    </h3>
+                </div>
+                </div>
+            </div>
+            <div className="col-lg-6 col-md-6">
+                <div className="project-block--style4">
+                <div className="project-block__media">
+                    <a href="project-details.html">
+                    <img
+                        className="project-block__img"
+                        src="img/project/project-4-5.jpg"
+                        alt="project details"
+                    />
+                    </a>
+                </div>
+                <div className="project-block__content">
+                    <span className="project-block__subtitle">CONSTRUCTION</span>
+                    <h3 className="project-block__title h4">
+                    <a
+                        className="project-block__title__link"
+                        href="project-details.html"
+                    >
+                        Park Offices
+                    </a>
+                    </h3>
+                </div>
+                </div>
+                <div className="project-block--style4">
+                <div className="project-block__media">
+                    <a href="project-details.html">
+                    <img
+                        className="project-block__img"
+                        src="img/project/project-4-6.jpg"
+                        alt="project details"
+                    />
+                    </a>
+                </div>
+                <div className="project-block__content">
+                    <span className="project-block__subtitle">CONSTRUCTION</span>
+                    <h3 className="project-block__title h4">
+                    <a
+                        className="project-block__title__link"
+                        href="project-details.html"
+                    >
+                        Road Construction
+                    </a>
+                    </h3>
+                </div>
+                </div>
+                <div className="project-block--style4">
+                <div className="project-block__media">
+                    <a href="project-details.html">
+                    <img
+                        className="project-block__img"
+                        src="img/project/project-4-7.jpg"
+                        alt="project details"
+                    />
+                    </a>
+                </div>
+                <div className="project-block__content">
+                    <span className="project-block__subtitle">CONSTRUCTION</span>
+                    <h3 className="project-block__title h4">
+                    <a
+                        className="project-block__title__link"
+                        href="project-details.html"
+                    >
+                        Visual Design
+                    </a>
+                    </h3>
+                </div>
+                </div>
+                <div className="project-block--style4">
+                <div className="project-block__media">
+                    <a href="project-details.html">
+                    <img
+                        className="project-block__img"
+                        src="img/project/project-4-8.jpg"
+                        alt="project details"
+                    />
+                    </a>
+                </div>
+                <div className="project-block__content">
+                    <span className="project-block__subtitle">CONSTRUCTION</span>
+                    <h3 className="project-block__title h4">
+                    <a
+                        className="project-block__title__link"
+                        href="project-details.html"
+                    >
+                        House Renovation
+                    </a>
+                    </h3>
+                </div>
+                </div>
+            </div>
+            </div>
+            <div className="row">
+            <div className="col-12 text-center">
+                <div className="vs-pagination">
+                <a href="#" className="pagi-btn">
+                    Prev
+                </a>
+                <ul>
+                    <li>
+                    <a href="#">1</a>
+                    </li>
+                    <li>
+                    <a href="#">2</a>
+                    </li>
+                    <li>
+                    <a href="#">3</a>
+                    </li>
+                    <li>
+                    <a href="#">...</a>
+                    </li>
+                    <li>
+                    <a href="#">16</a>
+                    </li>
+                </ul>
+                <a href="#" className="pagi-btn">
+                    next
+                </a>
+                </div>
+            </div>
+            </div>
+        </div>
+    </div>
+    </>
+  )
+}
